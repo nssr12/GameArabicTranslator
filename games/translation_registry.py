5f196055243ce0fdaeaa2f7f8182c66d66c0b058
nssr12 <nssr12@gmail.com>
2026-05-14 03:00:29 +0300

    Release app v2.0

--- games/translation_registry.py
+++ games/translation_registry.py
@@ -10,7 +10,7 @@ from typing import Optional, Dict
 MANIFEST_URL = (
     "https://raw.githubusercontent.com/nssr12/GameArabicTranslator/main/manifest.json"
 )
-APP_VERSION = "1.9"
+APP_VERSION = "2.0"
 
 
 def _version_gt(a: str, b: str) -> bool:
